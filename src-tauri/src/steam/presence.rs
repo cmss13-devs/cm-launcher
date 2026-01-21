57a87cbdb3ddb9316f935fdda3cb23cbb7011fc4
harryob <55142896+harryob@users.noreply.github.com>
2026-01-21 16:27:44 +0000

    handle connect button

--- src-tauri/src/steam/presence.rs
+++ src-tauri/src/steam/presence.rs
@@ -3,32 +3,26 @@ use steamworks::Client;
 pub fn set_playing_status(client: &Client, server_name: &str, player_count: u32) {
     let friends = client.friends();
 
-    if !friends.set_rich_presence("status", Some(&format!("Playing on {}", server_name))) {
-        eprintln!("Failed to set rich presence 'status'");
-    }
-    if !friends.set_rich_presence("players", Some(&player_count.to_string())) {
-        eprintln!("Failed to set rich presence 'players'");
-    }
-    if !friends.set_rich_presence("name", Some(server_name)) {
-        eprintln!("Failed to set rich presence 'name'");
-    }
-    if !friends.set_rich_presence("steam_display", Some("#Status_Playing")) {
-        eprintln!("Failed to set rich presence 'steam_display'");
-    }
+    friends.set_rich_presence("status", Some(&format!("Playing on {}", server_name)));
+    friends.set_rich_presence("connect", Some(server_name));
+
+    friends.set_rich_presence("players", Some(&player_count.to_string()));
+    friends.set_rich_presence("name", Some(server_name));
+
+    friends.set_rich_presence("steam_display", Some("#Status_Playing"));
+    friends.set_rich_presence("steam_player_group", Some(server_name));
+    friends.set_rich_presence("steam_player_group_size", Some(&player_count.to_string()));
 }
 
 pub fn set_launcher_status(client: &Client) {
+    clear_presence(client);
+
     let friends = client.friends();
 
-    if !friends.set_rich_presence("status", Some("In the Launcher")) {
-        eprintln!("Failed to set rich presence 'status'");
-    }
-    if !friends.set_rich_presence("steam_display", Some("#Status_Launcher")) {
-        eprintln!("Failed to set rich presence 'steam_display'");
-    }
+    friends.set_rich_presence("status", Some("In the Launcher"));
+    friends.set_rich_presence("steam_display", Some("#Status_Launcher"));
 }
 
-#[allow(dead_code)]
 pub fn clear_presence(client: &Client) {
     client.friends().clear_rich_presence();
 }
