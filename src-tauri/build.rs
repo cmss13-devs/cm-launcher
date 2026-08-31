665d31a49011783b0887027a4896187e268d8d23
harryob <55142896+harryob@users.noreply.github.com>
2026-08-31 17:21:18 +0100

    cm feature too

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
+    #[cfg(feature = "cm_ss13")]
+    if std::env::var_os("TAURI_CONFIG").is_none() {
+        if let Ok(config) = std::fs::read_to_string("tauri.cm.conf.json") {
+            println!("cargo:warning=Applying CM-SS13 config overlay");
+            std::env::set_var("TAURI_CONFIG", config);
+        }
+        println!("cargo:rerun-if-changed=tauri.cm.conf.json");
+    }
+
     tauri_build::build();
 }
